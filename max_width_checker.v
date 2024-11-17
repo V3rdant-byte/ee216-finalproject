e4378d22c5eb00a8fa57a1ee0957defbc55d2892
V3rdant-byte <yangtuo2001@163.com>
2024-11-16 16:57:20 -0800

    fix a typo

--- max_width_checker.v
+++ max_width_checker.v
@@ -7,11 +7,11 @@ module max_wdith_checker(
     output reg strike
 );
 
-wire real_en = clk && en;
+wire real_clk = clk && en;
 wire [7:0] sum_w = width_i + occupied_width;
 wire strike_w = (sum_w > 7'd127) ? 1'b1 : 1'b0;
 
-always @(posedge real_en or posedge rst) begin
+always @(posedge real_clk or posedge rst) begin
     if (rst) strike <= 1'b0;
     else strike <= strike_w;
 end
