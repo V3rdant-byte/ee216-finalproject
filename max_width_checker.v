0efd730a64f2d846a373492e1b04c0f0a7a02474
V3rdant-byte <yangtuo2001@163.com>
2024-11-17 12:20:14 -0800

    update enclk

--- max_width_checker.v
+++ max_width_checker.v
@@ -1,17 +1,15 @@
 module max_wdith_checker(
-    input clk,
-    input en,
+    input enclk,
     input rst,
     input [4:0] width_i,
     input [6:0] occupied_width,
     output reg strike
 );
 
-wire real_clk = clk && en;
 wire [7:0] sum_w = width_i + occupied_width;
 wire strike_w = (sum_w > 7'd127) ? 1'b1 : 1'b0;
 
-always @(posedge real_clk or posedge rst) begin
+always @(posedge enclk or posedge rst) begin
     if (rst) strike <= 1'b0;
     else strike <= strike_w;
 end
