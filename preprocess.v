71f9be30155a15e6ac37d1fb6d13b2212644e94e
Tianrun Gou <toukacoco66@gmail.com>
2024-11-17 20:23:19 -0800

    Update preprocess.v

--- preprocess.v
+++ preprocess.v
@@ -10,7 +10,7 @@ assign sub4 = height - 4;
 
 always@(posedge clk or posedge rst)begin
     if(rst)
-        addr <= 4'd4;
+        addr <= 4'd0;
     else
         addr <= (sub4[3:0] >= 9)? 9 : sub4[3:0];
 end
