3a87bd1909f36000d176d5f4cebc95f668b445f8
Fuyao Zhou <zhoufuyao7@gmail.com>
2024-12-01 16:39:50 -0800

    successful simulation after synthesis

--- pj_synthesis.tcl
+++ pj_synthesis.tcl
@@ -35,8 +35,8 @@ link
 set_operating_conditions -min ff1p16vn40c -max ss0p95v125c
 
 # Describe the clock waveform & setup operating conditions
-set Tclk_i 0.77
-set TCU  0.05
+set Tclk_i 0.82
+set TCU  0.1
 set IN_DEL 0.6
 set IN_DEL_MIN 0.3
 set OUT_DEL 0.6
